2521ff8bcb3e8c419547de5e213ab39faa78f0f7
Willian Felipe <w.felipebraz@gmail.com>
2023-07-11 15:55:37 -0300

    corrigido lint req 1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -6,18 +6,18 @@ import java.util.Scanner;
  * App.
  */
 public class App {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-//    String option = menu(scanner);
-    System.out.println("Digite a quantidade de atividades para cadastrar: ");
-    String quantity = scanner.nextLine();
-    int value = Integer.parseInt(quantity);
-    for (int index = 1; index <= value; index += 1) {
-      System.out.println("Digite o nome da atividade " + index + ":");
-      String activity = scanner.nextLine();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a quantidade de atividades para cadastrar:");
+        String quantity = scanner.nextLine();
+        int value = Integer.parseInt(quantity);
 
-      System.out.println("Digite o peso da atividade " + index + ":");
-      int peso = Integer.parseInt(scanner.nextLine());
+        for (int index = 1; index <= value; index += 1) {
+            System.out.println("Digite o nome da atividade " + index + ":");
+            String activity = scanner.nextLine();
+
+            System.out.println("Digite o peso da atividade " + index + ":");
+            int height = Integer.parseInt(scanner.nextLine());
+        }
     }
-  }
 }
