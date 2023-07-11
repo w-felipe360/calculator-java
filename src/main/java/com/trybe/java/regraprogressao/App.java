43dc071f8af5949fd55ee6779b8067a76586c236
Willian Felipe <w.felipebraz@gmail.com>
2023-07-11 14:56:12 -0300

    requisito 1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,23 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
- // testeddddddddddddddddd
-  /**
-   * Metodo main.
-   */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+//    String option = menu(scanner);
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    String quantity = scanner.nextLine();
+    int value = Integer.parseInt(quantity);
+    for (int index = 1; index <= value; index += 1) {
+      System.out.println("Digite o nome da atividade " + index + ":");
+      String activity = scanner.nextLine();
+
+      System.out.println("Digite o peso da atividade " + index + ":");
+      int peso = Integer.parseInt(scanner.nextLine());
+    }
   }
 }
